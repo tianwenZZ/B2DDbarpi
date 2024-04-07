75aad2faa554e45bdc18224f7ec3a1c9fd49b616
Tianwen Zhou <tianwen.zhou@cern.ch>
2024-04-07 16:08:38 +0800

    dv scripts for filtered MC

---
+++ production/job_submit/filtered_MC/get_decay_path.sh
@@ -0,0 +1,3 @@
+#setenv_lhcb_lxplus
+lb-dirac prod dirac-bookkeeping-decays-path 12197018 > 12197018_Bu_D0D0pi,K3Pi,KPi=sqDalitz.txt
+lb-dirac prod dirac-bookkeeping-decays-path 12197070 > 12197070_Bu_D0D0pi,KPi,K3Pi=sqDalitz.txt
